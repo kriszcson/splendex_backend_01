348a5850a48ebb50cc1b0cdfed511b11323baa48
Ábel Krisztián <krisztian@splendex.io>
2021-02-04 12:04:26 +0100

    fix: error handling

--- src/app.js
+++ src/app.js
@@ -7,10 +7,17 @@ app.use(express.json());
 app.use('/weather', routes);
 
 app.get('/', (req, res) => {
-    res.json({
-        message: 'Welcome to the REST API Validation with Express project!',
-    });
+    res.redirect('/weather');
 });
+
+app.get('/weather', (req, res) => {
+    res.json({ message: "Welcome to my weather API!" });
+});
+
+app.get("*", (req, res) => {
+    res.json({ error: "(404) Site not found!" });
+})
+
 app.set('port', process.env.PORT || 3000);
 
 // Start listening on our port.
--- src/routes.js
+++ src/routes.js
@@ -12,8 +12,11 @@ router.get('/city/:city?', (req, res) => {
     let city = helpers.getCity(req.params.city);
     let getCityURL = `${constants.openWeatherMap.BASE_URL}q=${city}&${constants.openWeatherMap.METRIC_VALUES}&appid=${constants.openWeatherMap.SECRET_KEY}`;
     request(getCityURL, (err, res2, body) => {
-        if (err) {
-            res.json({ error: err });
+        if (res2.statusCode == 404) {
+            res.json({ error: "(404) city not found!" });
+        }
+        if (res2.statusCode >= 500) {
+            res.json({ error: "Intenral server error!" });
         }
         data = JSON.parse(body);
         res.json(data);
@@ -28,15 +31,16 @@ router.get('/zip-code/:zipcode?', (req, res) => {
     }
     let getZipcodeURL = `${constants.openWeatherMap.BASE_URL}zip=${zipCode},hu&${constants.openWeatherMap.METRIC_VALUES}&appid=${constants.openWeatherMap.SECRET_KEY}`;
     request(getZipcodeURL, (err, res2, body) => {
-        if (err) {
-            res.json({ error: err });
+        if (res2.statusCode == 404) {
+            res.json({ error: "(404) zip-code not found!" });
+        }
+        if (res2.statusCode >= 500) {
+            res.json({ error: "Intenral server error!" });
         }
         data = JSON.parse(body);
         res.json(data);
     })
 })
-
-
 
 router.get('/coordinates/:lat?/:lon?', (req, res) => {
     let latitude = req.params.lat;
@@ -45,20 +49,20 @@ router.get('/coordinates/:lat?/:lon?', (req, res) => {
         latitude = 46.069333056;
         longitude = 18.226499094;
     }
+
     let getCoordinatesURL = `${constants.openWeatherMap.BASE_URL}lat=${latitude}&lon=${longitude}&${constants.openWeatherMap.METRIC_VALUES}&appid=${constants.openWeatherMap.SECRET_KEY}`;
     request(getCoordinatesURL, (err, res2, body) => {
-        if (err) {
-            res.json({ error: err });
+        if (res2.statusCode == 404) {
+            res.json({ error: "(404) Coordinates not found!" });
         }
+        if (res2.statusCode >= 500) {
+            res.json({ error: "Intenral server error!" });
+        }
+
         data = JSON.parse(body);
         res.json(data);
     })
 })
 
-router.get("*", (req, res) => {
-    res.render('404', {
-        title: "page not found"
-    })
-})
 
 module.exports = router;
